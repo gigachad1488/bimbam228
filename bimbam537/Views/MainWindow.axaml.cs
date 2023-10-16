4948c60c00e1dfa5618d6bb1f9ad67fb56ac4e40
gigachad1488 <iluhadolmatov@bk.ru>
2023-10-16 16:07:29 +0700

    mega theme ui update

--- bimbam537/Views/MainWindow.axaml.cs
+++ bimbam537/Views/MainWindow.axaml.cs
@@ -1,17 +1,137 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Styling;
+using Brush = Avalonia.Media.Brush;
+using Brushes = Avalonia.Media.Brushes;
+using Color = Avalonia.Media.Color;
 
 
 namespace bimbam537.Views;
 
 public partial class MainWindow : Window
 {
-
+    private bool isBlackTheme = false;
     
-    public MainWindow()
+
+public MainWindow()
     {
         InitializeComponent();
+        isBlackTheme = false;
+        ChangeTheme(new WhiteTheme());
+        table1Button.Click += delegate { ChangeTable(0); };
+        table2Button.Click += delegate { ChangeTable(1); };
+        table3Button.Click += delegate { ChangeTable(2); };
+        table4Button.Click += delegate { ChangeTable(3); };
+        
+        ChangeTable(0);
     }
+
+public void ThemeChanger(object? sender, RoutedEventArgs e)
+{
+    if (!isBlackTheme)
+    {
+        isBlackTheme = true;
+        ChangeTheme(new BlackTheme());
+    }
+    else
+    {
+        isBlackTheme = false;
+        ChangeTheme(new WhiteTheme());
+    }
+}
+
+    public void ChangeTheme(Theme theme)
+    {
+            TabsGrid.Background = new SolidColorBrush(theme.mainColor);
+            FunctionsGrid.Background = new SolidColorBrush(theme.mainColor);
+            DataGridPanel.Background = new SolidColorBrush(theme.mainColor);
+            DataGrid.Background = new SolidColorBrush(theme.secondaryColor);
+            DataGrid.Foreground = new SolidColorBrush(theme.textColor);
+            DataGrid.RowBackground = new SolidColorBrush(theme.secondaryColor);
+            
+            table1Button.Background = new SolidColorBrush(theme.elementsColor1);
+            table1Button.Foreground = new SolidColorBrush(theme.textColor);
+            table2Button.Background = new SolidColorBrush(theme.elementsColor1);
+            table2Button.Foreground = new SolidColorBrush(theme.textColor);
+            table3Button.Background = new SolidColorBrush(theme.elementsColor1);
+            table3Button.Foreground = new SolidColorBrush(theme.textColor);
+            table4Button.Background = new SolidColorBrush(theme.elementsColor1);
+            table4Button.Foreground = new SolidColorBrush(theme.textColor);
+            
+            addButton.Background = new SolidColorBrush(theme.elementsColor2);
+            addButton.Foreground = new SolidColorBrush(theme.textColor);
+            changeButton.Background = new SolidColorBrush(theme.elementsColor2);
+            changeButton.Foreground = new SolidColorBrush(theme.textColor);
+            deleteButton.Background = new SolidColorBrush(theme.elementsColor2);
+            deleteButton.Foreground = new SolidColorBrush(theme.textColor);
+            themeButton.Background = new SolidColorBrush(theme.elementsColor2);
+            themeButton.Foreground = new SolidColorBrush(theme.textColor);
+    }
+
+    public void ChangeTable(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                table1Button.BorderBrush = Brushes.Blue;
+                table2Button.BorderBrush = Brushes.Transparent;
+                table3Button.BorderBrush = Brushes.Transparent;
+                table4Button.BorderBrush = Brushes.Transparent;
+                break;
+            case 1:
+                table1Button.BorderBrush = Brushes.Transparent;
+                table2Button.BorderBrush = Brushes.Blue;
+                table3Button.BorderBrush = Brushes.Transparent;
+                table4Button.BorderBrush = Brushes.Transparent;
+                break;
+            case 2:
+                table1Button.BorderBrush = Brushes.Transparent;
+                table2Button.BorderBrush = Brushes.Transparent;
+                table3Button.BorderBrush = Brushes.Blue;
+                table4Button.BorderBrush = Brushes.Transparent;
+                break;
+            case 3:
+                table1Button.BorderBrush = Brushes.Transparent;
+                table2Button.BorderBrush = Brushes.Transparent;
+                table3Button.BorderBrush = Brushes.Transparent;
+                table4Button.BorderBrush = Brushes.Blue;
+                break;
+        }
+    }
+    
+}
+
+public interface Theme
+{
+    public Color mainColor { get; set; }
+    public Color secondaryColor { get; set; }
+    public Color elementsColor1 { get; set; }
+    public Color elementsColor2 { get; set; }
+    
+    public Color textColor { get; set; }
+}
+
+public class BlackTheme : Theme
+{
+    public Color mainColor { get; set; } = new(255, 26, 26, 29);
+    public Color secondaryColor { get; set; } = new(255, 78, 78, 80);
+    public Color elementsColor1 { get; set; } = new(255, 114, 34, 50);
+    public Color elementsColor2 { get; set; } = new(255, 195, 7, 63);
+
+    public Color textColor { get; set; } = Colors.White;
+}
+
+public class WhiteTheme : Theme
+{
+    public Color mainColor { get; set; } = new Color(255, 247, 249, 251);
+    public Color secondaryColor { get; set; } = new Color(255, 143, 193, 227);
+    public Color elementsColor1 { get; set; } = new Color(255, 104, 120, 100);
+    public Color elementsColor2 { get; set; } = new Color(255, 49, 112, 142);
+
+    public Color textColor { get; set; } = Colors.Black;
 }
---
+++ bimbam537/asd.cs
@@ -0,0 +1,19 @@
+namespace bimbam537;
+
+public class asd
+{
+    public string a1 { get; set; }
+    public string a2 { get; set; }
+    public string a3 { get; set; }
+    public string a4 { get; set; }
+    public string a5 { get; set; }
+
+    public asd(string a1, string a2, string a3, string a4, string a5)
+    {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+        this.a4 = a4;
+        this.a5 = a5;
+    }
+}
